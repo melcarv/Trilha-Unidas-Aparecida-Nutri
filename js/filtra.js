5ec6d374cb25636437bf0a6b287c05f047f7ea05
melcarv <melm.dev@gmail.com>
2022-01-24 18:25:13 -0300

    Filtrando pacientes da tabela

---
+++ js/filtra.js
@@ -0,0 +1,9 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+campoFiltro.addEventListener("input", function(){
+    var pacientes = document.querySelectorAll(".paciente");
+    for (i = 0; i < pacientes.length; i++){
+        var paciente = pacientes[i];
+        var tdNome = paciente.querySelector(".info-nome");
+        var nome = paciente.textContent;
+    }
+});
