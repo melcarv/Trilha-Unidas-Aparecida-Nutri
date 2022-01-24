07f7c2a6dabe12e23969eac4c6da038439dabe35
melcarv <melm.dev@gmail.com>
2022-01-24 18:53:53 -0300

    Implementação da lógica de filtragem

--- js/filtra.js
+++ js/filtra.js
@@ -1,9 +1,22 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
+
 campoFiltro.addEventListener("input", function(){
     var pacientes = document.querySelectorAll(".paciente");
-    for (i = 0; i < pacientes.length; i++){
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
-    }
+    if(this.value.length > 0){
+        for (i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = paciente.textContent;
+            if (nome != this.value){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else{
+        for(i = 0; i < pacientes.length; i++){
+            var paciente = paciente[i];
+            paciente.classList.remove("invisivel");
+        }
+    }  
 });
