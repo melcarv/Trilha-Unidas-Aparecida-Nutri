8acd7d3343faf0e3a6739fa6d9d3c37a33ff8983
melcarv <melm.dev@gmail.com>
2022-01-25 17:18:50 -0300

    Filtrando com expressão regular

--- js/filtra.js
+++ js/filtra.js
@@ -7,7 +7,8 @@ campoFiltro.addEventListener("input", function(){
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = paciente.textContent;
-            if (nome != this.value){
+            var expressao = new RegExp(this.value, "i");
+            if (!expressao.test(nome)){
                 paciente.classList.add("invisivel");
             }else{
                 paciente.classList.remove("invisivel");
@@ -15,7 +16,7 @@ campoFiltro.addEventListener("input", function(){
         }
     }else{
         for(i = 0; i < pacientes.length; i++){
-            var paciente = paciente[i];
+            var paciente = pacientes[i];
             paciente.classList.remove("invisivel");
         }
     }  
